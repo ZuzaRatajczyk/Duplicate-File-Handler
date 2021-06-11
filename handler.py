e12e7d55e769cfefb7843ebf6b3f6534004d3d33
Zuza <zuza.ratajczyk@gmail.com>
2021-06-11 09:01:39 +0200

    Reorganize project

--- handler.py
+++ handler.py
@@ -2,60 +2,53 @@ import sys
 import os
 
 
-def flipp_dict(init_dict):
-    flipped = {}
-    for key, value in init_dict.items():
-        if value not in flipped:
-            flipped[value] = [key]
-        else:
-            flipped[value].append(key)
-    return flipped
-
-
-def check_num_of_val(dict_of_files):
-    dict_to_print = {}
-    for key, val in dict_of_files.items():
-        if len(val) > 1:
-            dict_to_print[key] = val
-    return dict_to_print
+def create_dict_of_sizes(root_folder, ext):
+    dict_of_sizes = {}
+    for root, dirs, files in os.walk(root_folder):
+        for name in files:
+            if os.path.splitext(name)[1] == "." + ext:
+                size = os.path.getsize(os.path.join(root, name))
+                if size not in dict_of_sizes:
+                    dict_of_sizes[size] = [os.path.join(root, name)]
+                else:
+                    dict_of_sizes[size].append(os.path.join(root, name))
+            elif not ext:  # if ext is not specified create dict of all files
+                size = os.path.getsize(os.path.join(root, name))
+                if size not in dict_of_sizes:
+                    dict_of_sizes[size] = [os.path.join(root, name)]
+                else:
+                    dict_of_sizes[size].append(os.path.join(root, name))
+    return dict_of_sizes
 
 
 def print_sorted(dict_to_print, sort_option):
-    if int(sort_option) == 1:
-        sort_keys = sorted(dict_to_print, reverse=True)
+    if sort_option == 1:
+        bool_reverse = True
     else:
-        sort_keys = sorted(dict_to_print)
-    for key in sort_keys:
-        print(str(key) + " bytes")
-        print("\n".join(dict_to_print[key]))
+        bool_reverse = False
+    for key in sorted(dict_to_print.items(), reverse=bool_reverse):
+        if len(key[1]) > 1:
+            print(str(key[0]) + " bytes")
+            print("\n".join(key[1]))
 
 
 def main():
+    sorting_options = [1, 2]
+    root_folder = ""
     args = sys.argv
     try:
         root_folder = args[1]
     except IndexError:
         print("Directory is not specified")
-
     ext = input("Enter file format:\n ")
-    options = [1, 2]
-    dict_of_sizes = {}
     print("Size sorting options:\n1. Descending\n2. Ascending\n")
-    sorting_option = input("Enter a sorting option:\n")
-    while int(sorting_option) not in options:
+    sorting_option = input("Enter a sorting option (1 or 2):\n")
+    while int(sorting_option) not in sorting_options:
         print("Wrong option")
-        sorting_option = input("Enter a sorting option:\n")
+        sorting_option = input("Enter a sorting option (1 or 2):\n")
 
-    for root, dirs, files in os.walk(root_folder):
-        for name in files:
-            if os.path.splitext(name)[1] == "." + ext:
-                dict_of_sizes[os.path.join(root, name)] = os.path.getsize(os.path.join(root, name))
-            elif not ext:
-                dict_of_sizes[os.path.join(root, name)] = os.path.getsize(os.path.join(root, name))
-
-    dict_of_files = flipp_dict(dict_of_sizes)
-    dict_to_print = check_num_of_val(dict_of_files)
-    print_sorted(dict_of_files, sorting_option)
+    dict_of_sizes = create_dict_of_sizes(root_folder, ext)
+    print_sorted(dict_of_sizes, sorting_option)
 
 
 if __name__ == "__main__":
